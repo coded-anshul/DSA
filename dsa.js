543acf299540e58c65002f05efa1008e94b4923b
coded-anshul <anshuljain2106@gmail.com>
2025-10-30 16:03:03 +0530

    DAY01

---
+++ dsa.js
@@ -0,0 +1,50 @@
+//a = 10;
+//console.log(a);
+
+//sum of two integer
+// 
+// let a = 10 ;
+// let b = 10;
+// console.log(a+b);
+// 
+// let a1 = 10;
+// let b2 ="sum";
+// console.log(a1+b2);
+
+//string + string = string (concatenation)
+//string +number = string (concatenation)
+//num + num = num (arithamatic)
+
+// let a = 10;
+// let b = 20;
+// 
+// console.log("the sum of " + a + "and" + b + "is" + a + b);
+// 
+//The sum of 10 + and + b + is + a + b
+//The sum of 10 and + b + is + a + b
+//The sum of 10 and 20 + is + a + b
+//The sum of 10 and 20 is + a + b
+//The sum of 10 and 20 is 10 + b
+//The sum of 10 and 20 is 1020
+
+// console.log("the sum of " + a + "and" + b + "is" +( a + b));
+// 
+// let  c=Number(prompt("enter your number"));
+// console.log(c);
+
+
+// let a = 10;
+// let b = 20;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a,b);
+
+// [a,b]=[10,20];
+// [b,a]=[a,b]
+// console.log([a,b])
+
+// let a = 1;
+// let b= ++a;
+// console.log(a);
+// console.log(b);
