a15e7333aaab4713affb687fe4b60cd03b8180a5
coded-anshul <anshuljain2106@gmail.com>
2025-11-01 15:59:01 +0530

    CHOD BHANGDA

--- DAY02/day02.js
+++ DAY02/day02.js
@@ -9,7 +9,7 @@
 // console.log(Math.min(2,3));
 // console.log(Math.random());
 //console.log(Math.pow(2,3));
-console.log(Math.floor(Math.random()*900+100));
+//console.log(Math.floor(Math.random()*900+100));
 
 
 
